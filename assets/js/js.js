424ed78a4d721e42305d9e94c57309559a27e8fd
cla91 <83760330+cla91@users.noreply.github.com>
2022-12-13 13:18:29 +0100

    Update js.js

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,35 +1,74 @@
 const counterContainer = document.querySelector(".counter-container");
-        
-        function addElement(anchor, elem, classes, innerHTML, attribute, attributeValue) {
-            let newElem = document.createElement(elem);
-            newElem.classList.add(classes);
-            newElem.innerHTML = innerHTML;
-            anchor.append(newElem);
-            newElem.setAttribute(attribute, attributeValue);
-            return newElem; 
-        }
-        
-        const minusButton = addElement(counterContainer, "button", "operator", "-", "data-counter-operator", "minus");
-        const numberText = addElement(counterContainer, "div", "value", "0", "data-number", "");
-        const plusButton = addElement(counterContainer, "button", "operator", "+", "data-counter-operator", "plus");
-        const resetButton = addElement(counterContainer, "button", "reset", "Reset", "data-counter-reset", "");
 
-        counterContainer.addEventListener("click", changeValue);
+function addElement(anchor, elem, elemProperties) {
+  let newElem = document.createElement(elem);
+  
+  elemProperties.classes.length &&
+    newElem.classList.add(...elemProperties.classes);
 
-        function changeValue(event) {
-            let target = event.target.closest("BUTTON");
-            
-            switch(target) {
-                case plusButton:
-                    numberText.innerHTML++;
-                break;
+  newElem.innerHTML = elemProperties.innerHTML;
+  anchor.append(newElem);
 
-                case minusButton:
-                  numberText.innerHTML--;
-                break;
+  if (Object.entries(elemProperties.attributes).length != 0) {
+    for (let [attribute, attributeValue] of Object.entries(
+      elemProperties.attributes
+    )) {
+      newElem.setAttribute(attribute, attributeValue);
+    }
+  }
 
-                case resetButton:
-                    numberText.innerHTML = 0;
-                break;
-            }
-        }
+  return newElem;
+}
+
+const minusButtonProperties = {
+  innerHTML: "-",
+  classes: ["operator"],
+  attributes: {
+    "data-counter-operator": "minus",
+  },
+};
+const plusButtonProperties = {
+  innerHTML: "+",
+  classes: ["operator"],
+  attributes: {
+    "data-counter-operator": "plus",
+  },
+};
+const resetButtonProperties = {
+  innerHTML: "Reset",
+  classes: ["reset"],
+  attributes: {
+    "data-counter-reset": "",
+  },
+};
+const numberTextProperties = {
+  innerHTML: "0",
+  classes: ["value"],
+  attributes: {
+    "data-number": "",
+  },
+};
+const minusButton = addElement(counterContainer,"button", minusButtonProperties);
+const numberText = addElement(counterContainer, "div", numberTextProperties);
+const plusButton = addElement(counterContainer, "button", plusButtonProperties);
+const resetButton = addElement(counterContainer, "button", resetButtonProperties);
+
+counterContainer.addEventListener("click", changeValue);
+
+function changeValue(event) {
+  let target = event.target.closest("BUTTON");
+
+  switch (target) {
+    case plusButton:
+      numberText.innerHTML++;
+      break;
+
+    case minusButton:
+      numberText.innerHTML--;
+      break;
+
+    case resetButton:
+      numberText.innerHTML = 0;
+      break;
+  }
+}
